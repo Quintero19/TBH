7fb2001722736d4c04e51fdc4d4aba0b2b7918bf
Sebastian Roldan Daza <roldansebasdaza@gmail.com>
2025-09-10 21:37:34 -0500

    ...

--- src/pages/Auth/ActivateAccount.jsx
+++ src/pages/Auth/ActivateAccount.jsx
@@ -1,11 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "@/utils/api";
+import {useNavigate } from "react-router-dom";
 
 
 
 export default function ActivateAccount() {
   const { token } = useParams();
+  const navigate = useNavigate();
   const [status, setStatus] = useState("loading");
 
   useEffect(() => {
@@ -15,6 +17,7 @@ export default function ActivateAccount() {
 
           if (res.status === 200) {
             setStatus("success");
+            setTimeout(() => navigate("/login"), 2500);
           } else {
             setStatus("error");
           }
